5cbbed772f3c989541960e9077f9bb5758a0fdb3
Eunice Chew <eunice.chew.2021@scis.smu.edu.sg>
2023-03-21 17:39:46 +0800

    Profile header done

--- services/frontend/app/src/components/Profile/Profile.jsx
+++ services/frontend/app/src/components/Profile/Profile.jsx
@@ -10,54 +10,104 @@ export default function Profile() {
     return (
         <div className="grey-background">
             <div className="white-background">
-                <div className="banner-container">
-                    <img src={wave}>
-                    </img>
-                </div>
 
-                <div className="profile-header-wrapper">
-                    <div className="profile-picture-container">
-                        <img src={profilePic}></img>
+                <div className="profile-header">
+
+                    <div className="banner-container">
+                        <img src={wave}>
+                        </img>
                     </div>
 
-                    <div className="name-age-gender-buttons-wrapper">
-                        
-                        <div className="name-age-gender-wrapper">
-                            <text className="title-text" style={{lineHeight:"28px"}}>Jessica Jameson</text>
-                            <text className="grey-subtitle-text" style={{lineHeight:"28px"}}>24 years • Female</text>
+                    <div className="profile-header-main-info-and-buttons">
+                        <div className="profile-header-top-row">
+                            <div className="profile-picture-container">
+                                <img src={profilePic}></img>
+                            </div>
+
+                            <div className="name-age-gender-buttons-wrapper">
+                                
+                                <div className="name-age-gender-wrapper">
+                                    <text className="title-text" style={{lineHeight:"28px"}}>Jessica Jameson</text>
+                                    <text className="grey-subtitle-text" style={{lineHeight:"28px"}}>24 years • Female</text>
+                                </div>
+
+                                <div className="profile-buttons-wrapper">
+                                    <button className="yellow-profile-button-container" >
+                                        <FiBookmark/>
+                                        <text className="black-subtitle-text">
+                                            Save User
+                                        </text>
+                                    </button>
+
+                                    <button className="yellow-profile-button-container" >
+                                        <BiSend/>
+                                        <text className="black-subtitle-text">
+                                            Send Invite
+                                        </text>
+                                    </button>
+                                    <button className="transparent-profile-button-container">
+                                        <MdOutlineMoreVert/>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="profile-buttons-wrapper">
-                            <button className="yellow-profile-button-container" >
-                                <FiBookmark/>
-                                <text className="black-subtitle-text">
-                                    Save User
+                        <div className="profile-header-contact-details">
+                            <div className="social-media-detail">
+                                <i class="fa-brands fa-telegram" />
+                                <text className="grey-subtitle-text">
+                                    @JessyJ
                                 </text>
-                            </button>
+                            </div>
+                            <div className="social-media-detail">
+                                <i class="fa-brands fa-instagram" />
+                                <text className="grey-subtitle-text">
+                                    @JessyJami
+                                </text>
+                            </div>
+                            <div className="social-media-detail">
+                                <i class="fa-brands fa-facebook" />
+                                <text className="grey-subtitle-text">
+                                    facebook.com/doublej
+                                </text> 
+                            </div>
+                        </div>
 
-                            <button className="yellow-profile-button-container" >
-                                <BiSend/>
-                                <text className="black-subtitle-text">
-                                    Send Invite
+                        <div className="profile-header-description">
+                            <text className="black-subtitle-text">
+                                What’s up! I am an international student from America looking for someone to split rental costs with! 
+                                I am an avid vegan. 
+                                I am hoping to help local animal welfare groups by fostering cats when I move in :) 
+                            </text>
+                        </div>
+
+                        <div className="profile-header-mutuals-section">
+                            <text className="grey-body-text">
+                                You both follow
+                            </text>
+                            <div className="mutual-container">
+                                <i class="fa-brands fa-facebook"/>
+                                <text className="grey-body-text">
+                                    Felice Nevihat
                                 </text>
-                            </button>
-                            <button className="transparent-profile-button-container">
-                                <MdOutlineMoreVert/>
-                            </button>
+                            </div>
+                            <text className="grey-body-text"> 
+                                ,
+                            </text>
+                            <div className="mutual-container">
+                                <i class="fa-brands fa-instagram"/>
+                                <text className="grey-body-text">
+                                    Mark Imar
+                                </text>
+                            </div>
+                            <div className="mutual-container">
+                                <text className="grey-body-text">
+                                    + 3 others.
+                                </text>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="social-media-wrapper">
-                    <i class="fa-brands fa-telegram" />
-                    <text className="grey-subtitle-text">
-                        @JessyJ
-                    </text>
-                    <i class="fa-brands fa-instagram" />
-                    <i class="fa-brands fa-facebook" />
-                </div>
-
-
 
             </div>
         </div>
